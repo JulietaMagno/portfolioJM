d084facd1ad903fb68efaed4b56dae695d7a126e
Julieta <magnojulieta@gmail.com>
2024-03-28 14:27:05 +0100

    padding pages

--- src/app/my-certificates/page.tsx
+++ src/app/my-certificates/page.tsx
@@ -6,8 +6,10 @@ import React from 'react';
 
 const Page = () => {
   return (
-    <div className='w-screen h-screen flex flex-col items-center justify-center bg-gradient-to-r from-[#2A213D] to-black'>
-      <div className='w-screen flex items-center justify-center overflow-y-auto mt-[90px] mb-[70px]'>
+    <div 
+    style={{ paddingBottom: '70px' }}
+    className='w-screen h-screen flex flex-col items-center justify-center bg-gradient-to-r from-[#2A213D] to-black'>
+      <div className='w-screen flex items-center justify-center overflow-y-auto mt-[90px]'>
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 max-w-[93%] max-h-[100%] pt-4'>
           {Certificates.map((certificate, index) => (
               <CertificateCard
--- src/app/my-proyects/page.tsx
+++ src/app/my-proyects/page.tsx
@@ -6,8 +6,10 @@ import React from 'react';
 
 const Page = () => {
   return (
-    <div className='w-screen h-screen flex flex-col items-center justify-center bg-gradient-to-r from-[#2A213D] to-black'>
-      <div className='w-screen flex items-center justify-center overflow-y-auto mt-[100px] mb-[70px]'>
+    <div 
+    style={{ paddingBottom: '70px' }}
+    className='w-screen h-screen flex flex-col items-center justify-center bg-gradient-to-r from-[#2A213D] to-black'>
+      <div className='w-screen flex items-center justify-center overflow-y-auto mt-[100px]'>
         <div className='grid grid-cols-1 gap-6 max-w-[93%] max-h-[100%] py-2'>
           {Proyects.map((proyect, index) => (
               <ProyectsCard
