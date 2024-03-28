17068d545643947fc6580aab4c56dadb4e925210
Julieta <magnojulieta@gmail.com>
2024-03-28 20:35:14 +0100

    contact

--- src/app/contact-me/page.tsx
+++ src/app/contact-me/page.tsx
@@ -8,7 +8,7 @@ const Page = () => {
       className="w-screen h-screen flex items-center justify-center bg-cover bg-center"
     >
       <div 
-        className="h-[60%] w-[80%] relative  rounded-xl shadow-xl shadow-pink-600 bg-gradient-to-tr from-[#2A213D] to-black">
+        className="h-[60%] min-h-[440px] w-[80%] relative  rounded-xl shadow-xl shadow-pink-600 bg-gradient-to-tr from-[#2A213D] to-black mt-[90px]">
         <div className="absolute bottom-20 ml-10 md:ml-20 w-[80%] md:w-[60%]">
           <ContactForm />
         </div>
